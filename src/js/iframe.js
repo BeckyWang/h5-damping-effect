b6a3096991f39c4620d7c0d865250601db03e42b
BeckyWang <wangbeibei01018@163.com>
2018-06-25 10:52:14 +0800

    增加ifame相关文件

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -1,5 +1,54 @@
 $(document).ready(() => {
-    $(document).on('touchstart', e => {
-        console.log(e);
+    const RANGE_TOP = 100;
+
+    let start = null,
+        last_distance = 0;
+
+    $('.pageB').on('touchstart', e => {
+        start = e.changedTouches[0].pageY;
+    });
+
+    $('.pageB').on('touchmove', e => {
+        const cur_move = e.changedTouches[0].pageY - start;
+        const move_distance = last_distance + cur_move;
+
+        $('.wrapper').css({
+            'transform': `translateY(${move_distance}px)`,
+            'transition-duration': '0ms'
+        });
+
+        if (move_distance > 0 && move_distance < RANGE_TOP) {
+            $('.head-text').html('下拉，返回商品基本信息');
+        } else if (move_distance >= RANGE_TOP) {
+            $('.head-text').html('释放，查看商品基本信息');
+        }
+    });
+
+    $('.pageB').on('touchend', e => {
+        const cur_move = e.changedTouches[0].pageY - start;
+        last_distance += cur_move;
+        const slideHeight = $('.slide').outerHeight() - $(window).height();
+
+        if (last_distance <= -slideHeight) {
+            $('.wrapper').css({
+                'transform': `translateY(${-slideHeight}px)`,
+                'transition-duration': '400ms'
+            });
+            last_distance = -slideHeight;
+        } else if (last_distance > 0 && last_distance < RANGE_TOP) {
+            $('.wrapper').css({
+                'transform': 'translateY(0)',
+                'transition-duration': '400ms'
+            });
+            last_distance = 0;
+        } else if (last_distance >= RANGE_TOP) {
+            //切换到上一个页面
+            window.parent.postMessage('prev', '*');
+            $('.head-text').html('下拉，返回商品基本信息');
+            $('.wrapper').css({
+                'transform': 'translateY(0)',
+                'transition-duration': '0ms'
+            });
+        }
     });
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 import '../index.html';
-import '../scss/index.scss';
+import '../css/index.scss';
+import '../css/iframe.css';
 
 $(document).ready(() => {
-    const RANGE_BOTTOM = 100,
-        RANGE_TOP = 100;
-    const clientWidth = $(window).width();
+    const RANGE_BOTTOM = 100;
+    const clientWidth = $(window).width(),
+        clientHeight = $(window).height();
 
     let slideHeight = $('.slide').outerHeight() - $(window).height(),
         start = null,
@@ -28,8 +29,6 @@ $(document).ready(() => {
             $('.loading').hide();
             $('.iframe-info').show();
             iframe.height(getIframeHeight(iframe[0]));
-            slideHeight = $('.slide').outerHeight() - $(window).height();
-            last_distance = 0;
         });
     };
 
@@ -73,11 +72,14 @@ $(document).ready(() => {
             //切换到下一个页面
             $('.pageA').hide();
             $('.pageB').show();
-            $('.pageB .head-text').html('下拉，返回商品基本信息');
+            $('.wrapper').css({
+                'transform': 'translateY(0)',
+                'transition-duration': '0ms'
+            });
+
             //获取富文本内容，如果有直接显示，否则显示loading，再异步获取
-            if (iframeHtml) {
-                showIframe();
-            } else {
+            if (!iframeHtml) {
+                $('.loading').show();
                 $.ajax({
                     url: '/learning/api/v1/iframe/111',
                     type: 'get',
@@ -90,67 +92,43 @@ $(document).ready(() => {
                                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                                 <title>Detail Page</title>
                                 <meta name="viewport" content="width=device-width, initial-scale=1">
+                                <link rel="stylesheet" href="./css/iframe.css">
                                 <style>
-                                    * {margin: 0;padding: 0;}
-                                    body {background-color: #fff;}        
+                                    body {height: ${clientHeight}px;}        
                                     img {max-width: ${clientWidth}px;}
                                 </style>
                             </head>
-                            <body>${result.content}</body>
-                            <script type="text/javascript" src="http://localhost:8088/dist/lib/jquery.min.js"></script>
-                            <script type="text/javascript" src="./src/js/iframe.js"></script>
+                            <body>
+                                <div class="container">
+                                    <div class="wrapper">
+                                        <div class="slide">
+                                            <div class="head-more">
+                                                <p class="head-text">下拉，返回商品基本信息</p>
+                                            </div>
+                                            <div class="pageB">
+                                                ${result.content}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </body>
+                            <script type="text/javascript" src="./lib/jquery.min.js"></script>
+                            <script type="text/javascript" src="./js/iframe.js"></script>
                             </html>
                             `;
                         showIframe();
                     }
                 });
             }
-
-            $('.wrapper').css({
-                'transform': 'translateY(0)',
-                'transition-duration': '0ms'
-            });
         }
     });
 
-    $('.pageB').on('touchstart', e => {
-        start = e.changedTouches[0].pageY;
+    $(window).resize(() => {
+        slideHeight = $('.slide').outerHeight() - $(window).height();
     });
 
-    $('.pageB').on('touchmove', e => {
-        const cur_move = e.changedTouches[0].pageY - start;
-        const move_distance = last_distance + cur_move;
-
-        $('.wrapper').css({
-            'transform': `translateY(${move_distance}px)`,
-            'transition-duration': '0ms'
-        });
-
-        if (move_distance > 0 && move_distance < RANGE_TOP) {
-            $('.pageB .head-text').html('下拉，返回商品基本信息');
-        } else if (move_distance >= RANGE_TOP) {
-            $('.pageB .head-text').html('释放，查看商品基本信息');
-        }
-    });
-
-    $('.pageB').on('touchend', e => {
-        const cur_move = e.changedTouches[0].pageY - start;
-        last_distance += cur_move;
-
-        if (last_distance <= -slideHeight) {
-            $('.wrapper').css({
-                'transform': `translateY(${-slideHeight}px)`,
-                'transition-duration': '400ms'
-            });
-            last_distance = -slideHeight;
-        } else if (last_distance > 0 && last_distance < RANGE_TOP) {
-            $('.wrapper').css({
-                'transform': 'translateY(0)',
-                'transition-duration': '400ms'
-            });
-            last_distance = 0;
-        } else if (last_distance >= RANGE_TOP) {
-            //切换到上一个页面
+    window.addEventListener('message', rs => {
+        if (rs.data === 'prev') {
             $('.pageB').hide();
             $('.pageA').show();
             $('.pageA .foot-text').html('继续上拉查看商品详情');
@@ -164,12 +142,4 @@ $(document).ready(() => {
             });
         }
     });
-
-    $(window).resize(() => {
-        slideHeight = $('.slide').outerHeight() - $(window).height();
-    });
 });
-
-// if (module.hot) {
-//     module.hot.accept();
-// }
